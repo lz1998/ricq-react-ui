6bcb2d870079db2c1e2437588b43ec98df9fae71
lz1998 <875543533@qq.com>
2022-06-19 19:01:25 +0800

    plugin api

--- src/api/plugin.ts
+++ src/api/plugin.ts
@@ -1,1 +1,34 @@
-export {}
+import {get, post} from "./index";
+
+export interface SavePluginRequest {
+  name: string;
+  plugin: Plugin;
+}
+
+export interface Plugin {
+  disabled: boolean;
+  urls: string[];
+}
+
+export interface DeletePluginRequest {
+  name: string;
+}
+
+export interface BaseResponse {
+}
+
+export interface ListPluginResponse {
+  plugins: Map<string, Plugin>;
+}
+
+export const pluginDelete = async (req: DeletePluginRequest): Promise<BaseResponse> => {
+  return await post("/plugin/delete", req)
+}
+
+export const pluginSave = async (req: SavePluginRequest): Promise<BaseResponse> => {
+  return await post("/plugin/save", req)
+}
+
+export const pluginList = async (): Promise<ListPluginResponse> => {
+  return await get("/plugin/list")
+}
